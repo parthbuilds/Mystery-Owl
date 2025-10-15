5753c0e9394f6078b06b503eaab4fd71bc1979b9
Parth pandey <parthbuilds@gmail.com>
2025-10-15 16:10:41 +0530

    initial commit

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from "vite";
+import handlebars from "vite-plugin-handlebars";
+
+export default defineConfig({
+  root: "source", 
+  build: {
+    outDir: "../build",          
+    emptyOutDir: true
+  },
+  plugins: [
+    handlebars({
+      partialDirectory: "source/partials",
+      context: {
+        title: "Mystery Owl",
+        tagline: "Unravel the mystery before time runs out!"
+      }
+    })
+  ],
+  server: {
+    open: true
+  }
+});
